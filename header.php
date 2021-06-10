7a30b828ef24d55482028c4d798f499b8865c9d2
宮下　大樹 <miyashita_hiroki@TCMMAC2021-0009noMacBook-Pro.local>
2021-06-10 17:36:02 +0900

    refactor: change the file name of css

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 
-<link href="./styles/custom.scss" rel="stylesheet">
+<link href="./styles/main.scss" rel="stylesheet">
 
 <body>
 <div class="color-mode">
